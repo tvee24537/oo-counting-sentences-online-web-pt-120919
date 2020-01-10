4478867537b949df8144fcf6344b6d443384ebbd
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-01-10 22:18:42 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,5 +16,7 @@ class String
 
   def count_sentences
     self.split(/[.!?]/).reject {|x| x.empty?}.size
+    #split input string and put into array, remove empty values like "...", counts remaining value in array
+    
   end
 end
