c2d7495741c1b3ffb233e71efae9a014207ade85
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-01-10 22:18:56 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[.!?]/).reject {|x| x.empty?}.size
+    #split input string and put into array, remove empty values like "...", counts remaining value in array
+    
   end
 end
